31c777fa26a3e0bde31097b27e735aec4b27c86f
Abdurrahman BULUT <abdurrahmanbulut@yahoo.com>
2021-11-07 23:14:42 +0300

    promotions slider added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,45 +9,93 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex=0;
-  PageController pageController=PageController();
-  void onTapped(int index){
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
+  void onTapped(int index) {
     setState(() {
-      _selectedIndex=index;
+      _selectedIndex = index;
     });
-    pageController.animateToPage(index, duration: Duration(milliseconds: 100), curve: Curves.easeIn);
+    pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 100), curve: Curves.easeIn);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.amber,
-        title: Text('GROCERY APP',style: TextStyle(color:Colors.black,fontFamily: 'YOUR_FONT_FAMILY')),
+        title: const Text('GROCERY APP',
+            style:
+                TextStyle(color: Colors.black, fontFamily: 'YOUR_FONT_FAMILY')),
         centerTitle: true,
       ),
       body: PageView(
         controller: pageController,
         children: [
-          Container(color:Colors.white,),
-          Container(color:Colors.black),
-          Container(color:Colors.red),
-          Container(color:Colors.orange),
-          Container(child: AccountScreen(),),
+          Column(children: const [PromotionList()]),
+          Container(color: Colors.black),
+          Container(color: Colors.red),
+          Container(color: Colors.orange),
+          const AccountScreen()
         ],
-
       ),
-      bottomNavigationBar: BottomNavigationBar(items:const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label:'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label:'Search'),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label:'Cart'),
-        BottomNavigationBarItem(icon: Icon(Icons.campaign), label:'Campaigns'),
-        BottomNavigationBarItem(icon: Icon(Icons.menu), label:'Account'),
-      ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.campaign), label: 'Campaigns'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Account'),
+        ],
         currentIndex: _selectedIndex,
-        selectedItemColor:Colors.amber,unselectedItemColor: Colors.black ,
-        onTap:  onTapped,),
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.black,
+        onTap: onTapped,
+      ),
     );
+  }
+}
 
+class PromotionList extends StatefulWidget {
+  const PromotionList({Key? key}) : super(key: key);
+
+  @override
+  _PromotionListState createState() => _PromotionListState();
+}
+
+class _PromotionListState extends State<PromotionList> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: PageView.builder(
+        itemCount: 10,
+        controller: PageController(viewportFraction: 0.95),
+        itemBuilder: (_, i) {
+          return Transform.scale(
+            scale: 1.05,
+            child: TextButton(
+              onPressed: () {},
+              child: Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.amberAccent, width: 0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    "Promotion ${i + 1}",
+                    style: const TextStyle(fontSize: 32),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
