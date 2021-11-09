8b26570868aa18d5a19c7d76d7d68bc17518e09f
Abdurrahman BULUT <abdurrahmanbulut@yahoo.com>
2021-11-09 13:19:37 +0300

    bottom navigation bar improved

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/screens/account_screen.dart';
+import 'package:grocery_app/screens/search_screen.dart';
+import 'search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,51 +12,17 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  PageController pageController = PageController();
+
   void onTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-    pageController.animateToPage(index,
-        duration: const Duration(milliseconds: 100), curve: Curves.easeIn);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.amber,
-        title: const Text('GROCERY APP',
-            style:
-                TextStyle(color: Colors.black, fontFamily: 'YOUR_FONT_FAMILY')),
-        centerTitle: true,
-      ),
-      body: PageView(
-        controller: pageController,
-        children: [
-          Column(children: const [PromotionList()]),
-          Container(color: Colors.black),
-          Container(color: Colors.red),
-          Container(color: Colors.orange),
-          const AccountScreen()
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart), label: 'Cart'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.campaign), label: 'Campaigns'),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Account'),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber,
-        unselectedItemColor: Colors.black,
-        onTap: onTapped,
-      ),
+    return const Scaffold(
+      body: BottomNavBar(),
     );
   }
 }
@@ -95,6 +63,64 @@ class _PromotionListState extends State<PromotionList> {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    Column(children: const [PromotionList()]),
+    const SearchPage(),
+    Container(color: Colors.red),
+    Container(color: Colors.orange),
+    const AccountScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.amber,
+        title: const Text('GROCERY APP',
+            style:
+                TextStyle(color: Colors.black, fontFamily: 'YOUR_FONT_FAMILY')),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.campaign), label: 'Campaigns'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Account'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
       ),
     );
   }
