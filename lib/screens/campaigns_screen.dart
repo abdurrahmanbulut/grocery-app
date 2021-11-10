9112c8a890c8ca46574d34149ccf7f7e870e6a47
Abdurrahman BULUT <abdurrahmanbulut@yahoo.com>
2021-11-10 17:29:09 +0300

    campaigns page added

---
+++ lib/screens/campaigns_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class Campaigns extends StatefulWidget {
+  const Campaigns({Key? key}) : super(key: key);
+
+  @override
+  _CampaignsState createState() => _CampaignsState();
+}
+
+class _CampaignsState extends State<Campaigns> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        addAutomaticKeepAlives: true,
+        children: [
+          buildCampaignContainer(1),
+          buildCampaignContainer(2),
+          buildCampaignContainer(3),
+          buildCampaignContainer(4),
+          buildCampaignContainer(5),
+        ],
+      ),
+    );
+  }
+
+  Container buildCampaignContainer(int numberOfCampaing) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.amber,
+      ),
+      width: 100,
+      height: 200,
+      child: Center(
+        child: Text(
+          "Campaign$numberOfCampaing",
+          style: const TextStyle(fontSize: 20.0),
+        ),
+      ),
+    );
+  }
+}
