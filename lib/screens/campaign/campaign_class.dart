eb40d89faa96713ee966ce3897fd1f6dd40cfe82
alperenkaracete <m.alperenk@gmail.com>
2022-01-19 23:36:27 +0300

    campaign screen updates

--- lib/screens/campaign/campaign_class.dart
+++ lib/screens/campaign/campaign_class.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Campaign {
-  final String image, title, description;
+  final String image, title, description, product;
 
   Campaign({
     required this.image,
     required this.description,
     required this.title,
+    required this.product,
   });
 }
 
@@ -14,25 +15,31 @@ List<Campaign> campaigns = [
   Campaign(
       image: "assets/images/chocolate.jpg",
       description: "Only for this weekend!",
-      title: "%20 Sale in Chocolates!"),
+      title: "%20 Sale in Chocolates!",
+      product: "Chocolate"),
   Campaign(
       image: "assets/images/milk.jpg",
       description: "Only for this weekend!",
-      title: "%30 Sale in Milks!"),
+      title: "%30 Sale in Milks!",
+      product: "Tea"),
   Campaign(
       image: "assets/images/tomatoes.jpg",
       description: "Only for this weekend!",
-      title: "%40 Sale in Tomatoes!"),
+      title: "%40 Sale in Tomatoes!",
+      product: "Ayran"),
   Campaign(
       image: "assets/images/potatoes.jpg",
       description: "Only for this weekend!",
-      title: "%50 Sale in Potatoes!"),
+      title: "%50 Sale in Potatoes!",
+      product: "Coffe"),
   Campaign(
       image: "assets/images/chicken.jpg",
       description: "Only for this weekend!",
-      title: "%60 Sale in Chickens!"),
+      title: "%60 Sale in Chickens!",
+      product: "Fruit Juice"),
   Campaign(
       image: "assets/images/meat.jpg",
       description: "Only for this weekend!",
-      title: "%70 Sale in Meats!"),
+      title: "%70 Sale in Meats!",
+      product: "Coca Cola")
 ];
